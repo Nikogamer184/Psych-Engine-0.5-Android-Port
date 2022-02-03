c13f20c7fe2e8b5e0c3a6547c8774a34f6d52f55
Nikogamer184 <98852651+Nikogamer184@users.noreply.github.com>
2022-02-03 17:45:34 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3297,7 +3297,7 @@ class PlayState extends MusicBeatState
 			spawnNoteSplashOnNote(note);
 		}
 
-		if(!practiceMode && !cpuControlled) {
+		if(!practiceMode) {
 			songScore += score;
 			songHits++;
 			totalPlayed++;
